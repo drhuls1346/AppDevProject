721b25d26a2fefb4ef94b99fdb22ddcd261f4aec
drhuls1346 <drhuls1346@ung.edu>
2014-05-02 16:23:25 -0400

    In this commit, I have made different pages of the inventory application. There is now an add item, remove item, update item, check in, and check out page. I made the home page display all of the units that are in the inventory, I made the check out page display only the items that are currently checked in, and I made the check in page display only the items that are currently checked out. I had to change the home, check out, and check in pages to list views. Zac reran syncdb and the admin page works now. I also added links on the home page to all of the other pages that I created today.

--- Inventory/tests.py
+++ Inventory/tests.py
@@ -7,7 +7,7 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from django.core.urlresolvers import resolve, reverse
-from.views import HomeView
+from.views import HomeView, Add_ItemView, Remove_ItemView, Update_ItemView, Check_Out_ItemView, Check_In_ItemView
 
 
 class urlTests(TestCase):
@@ -15,3 +15,33 @@ class urlTests(TestCase):
         home = resolve(reverse('inv_templates:home'))
         return self.assertEqual(home.func.__name__,
                                 HomeView.__name__)
+
+class urlTests(TestCase):
+    def test_add_item_url(self):
+        add_item = resolve(reverse('inv_templates:add_item'))
+        return self.assertEqual(add_item.func.__name__,
+                                Add_ItemView.__name__)
+
+class urlTests(TestCase):
+    def test_remove_item_url(self):
+        remove_item = resolve(reverse('inv_templates:remove_item'))
+        return self.assertEqual(remove_item.func.__name__,
+                                Remove_ItemView.__name__)
+
+class urlTests(TestCase):
+    def test_update_item_url(self):
+        update_item = resolve(reverse('inv_templates:update_item'))
+        return self.assertEqual(update_item.func.__name__,
+                                Update_ItemView.__name__)
+
+class urlTests(TestCase):
+    def test_check_out_item_url(self):
+        check_out_item = resolve(reverse('inv_templates:check_out_item'))
+        return self.assertEqual(check_out_item.func.__name__,
+                                Check_Out_ItemView.__name__)
+
+class urlTests(TestCase):
+    def test_check_in_item_url(self):
+        check_in_item = resolve(reverse('inv_templates:check_in_item'))
+        return self.assertEqual(check_in_item.func.__name__,
+                                Check_in_ItemView.__name__)
--- Inventory/views.py
+++ Inventory/views.py
@@ -3,5 +3,32 @@ from .models import GPS
 
 # Create your views here.
 
-class HomeView(generic.TemplateView):
+class HomeView(generic.ListView):
+    model = GPS
     template_name = 'inv_templates/home.html'
+    context_object_name = 'unit'
+
+class Add_ItemView(generic.TemplateView):
+    model = GPS
+    template_name = 'inv_templates/add_item.html'
+
+class Remove_ItemView(generic.ListView):
+    model = GPS
+    template_name = 'inv_templates/remove_item.html'
+    context_object_name = 'unit'
+
+class Update_ItemView(generic.TemplateView):
+    model = GPS
+    template_name = 'inv_templates/update_item.html'
+
+class Check_Out_ItemView(generic.ListView):
+    model = GPS
+    template_name = 'inv_templates/check_out_item.html'
+    context_object_name = 'checkedin_units'
+    queryset = GPS.objects.filter(status=False)
+
+class Check_In_ItemView(generic.ListView):
+    model = GPS
+    template_name = 'inv_templates/check_in_item.html'
+    context_object_name = 'checkedout_units'
+    queryset = GPS.objects.filter(status=True)
